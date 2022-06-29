7b254a44695d410d68dcc876ee64c35a8099eefa
tres-caras <severinidiegoignacio@gmail.com>
2022-06-29 16:40:18 -0300

    animations

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="Interactive-colors">
           Bienvenidos <code>a</code> tres caras!
         </p>
         <a
